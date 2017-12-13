3968e556e0431dea19d8f26a9057b8025b0bea1e
Vince Reuter <vince.reuter@gmail.com>
2017-12-13 13:35:22 -0500

    integrate use of protocol interface for looper-specific version of pep project

--- looper/project.py
+++ looper/project.py
@@ -1,10 +1,19 @@
 """ Looper version of NGS project model. """
 
+from collections import defaultdict
+import logging
+import os
 import pep
+from pep.utils import alpha_cased
+from .protocol_interface import ProtocolInterface
 
 
 __author__ = "Vince Reuter"
 __email__ = "vreuter@virginia.edu"
+
+
+
+_LOGGER = logging.getLogger(__name__)
 
 
 
@@ -27,6 +36,8 @@ class Project(pep.Project):
                 config_file, subproject=subproject, 
                 no_environment_exception=RuntimeError,
                 no_compute_exception=RuntimeError, **kwargs)
+        self.interfaces_by_protocol = \
+            process_pipeline_interfaces(self.metadata.pipelines_dir)
 
 
     @property
@@ -57,3 +68,28 @@ class Project(pep.Project):
         proj_root_path, _ = os.path.split(metadata_folder_path)
         _, proj_root_name = os.path.split(proj_root_path)
         return proj_root_name
+
+
+def process_pipeline_interfaces(pipeline_interface_locations):
+    """
+    Create a ProtocolInterface for each pipeline location given.
+
+    :param Iterable[str] pipeline_interface_locations: locations, each of
+        which should be either a directory path or a filepath, that specifies
+        pipeline interface and protocol mappings information. Each such file
+        should be have a pipelines section and a protocol mappings section
+        whereas each folder should have a file for each of those sections.
+    :return Mapping[str, Iterable[ProtocolInterface]]: mapping from protocol
+        name to interface(s) for which that protocol is mapped
+    """
+    interface_by_protocol = defaultdict(list)
+    for pipe_iface_location in pipeline_interface_locations:
+        if not os.path.exists(pipe_iface_location):
+            _LOGGER.warn("Ignoring nonexistent pipeline interface "
+                         "location: '%s'", pipe_iface_location)
+            continue
+        proto_iface = ProtocolInterface(pipe_iface_location)
+        for proto_name in proto_iface.protomap:
+            _LOGGER.log(5, "Adding protocol name: '%s'", proto_name)
+            interface_by_protocol[alpha_cased(proto_name)].append(proto_iface)
+    return interface_by_protocol
