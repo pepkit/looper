58b46cd79496c2ea3aee43dda1a0555c7f8c24dc
jpsmith5 <jasonsmith@virginia.edu>
2018-06-15 09:51:23 -0400

    Switch object pages to bulleted lists

--- looper/looper.py
+++ looper/looper.py
@@ -692,17 +692,17 @@ class Summarizer(Executor):
                                             label=str(row['sample_name'])))
                         # Otherwise treat as a link
                         else:
-                            links.append(OBJECTS_LINK.format(
-                                            path=page_relpath,
+                            links.append(GENERIC_LIST_ENTRY.format(
+                                            page=page_relpath,
                                             label=str(row['sample_name'])))
                     # If no image file present, it's just a link
                     else:
-                        links.append(OBJECTS_LINK.format(
-                                        path=page_relpath,
+                        links.append(GENERIC_LIST_ENTRY.format(
+                                        page=page_relpath,
                                         label=str(row['sample_name'])))
-                html_file.write(OBJECTS_LIST_HEADER)
+                html_file.write(GENERIC_LIST_HEADER)
                 html_file.write("\n".join(links))
-                html_file.write(OBJECTS_LIST_FOOTER)
+                html_file.write(GENERIC_LIST_FOOTER)
                 html_file.write("\t\t\t<hr>\n")
                 html_file.write("\n".join(figures))
                 html_file.write("\t\t\t<hr>\n")
@@ -889,7 +889,6 @@ class Summarizer(Executor):
                 # html_file.write(TABLE_COLS_FOOTER)
 
                 # Produce table rows
-                html_file.write(TABLE_ROW_HEADER)
                 for key, value in sample_stats.items():
                     # Treat sample_name as a link to sample page
                     if key == 'sample_name':
@@ -908,7 +907,7 @@ class Summarizer(Executor):
                             row_name=str(key),
                             row_val=str(value)))
                 html_file.write(TABLE_FOOTER)
-                html_file.write("\t\t<hr>\n")
+                html_file.write("\t  <hr>\n")
 
                 # Add all the objects for the current sample
                 html_file.write("\t\t<div class='container-fluid'>\n")
@@ -1042,25 +1041,30 @@ class Summarizer(Executor):
                 for iface in ifaces:
                     pl = iface.fetch_pipelines(protocol)
                     summary_results = iface.get_attribute(pl, "summary_results")
-                    if not summary_results: continue
                     for result in summary_results:
                         caption = str(result['caption'])
                         file_path = str(result['path'])
-                        thumbnail_path = str(result['thumbnail_path'])
-
-                        file_path_formatted = file_path.format(**self.prj)
-                        thumbnail_path_formatted = thumbnail_path.format(**self.prj)
-                        
-                        file_path_abs = os.path.join(self.prj.metadata.output_dir, file_path_formatted)
-                        thumbnail_path_abs = os.path.join(self.prj.metadata.output_dir, thumbnail_path_formatted)
-
-                        if os.path.isfile(file_path_abs):
-                            if os.path.isfile(thumbnail_path_abs):
+                        img_path = str(result['thumbnail_path'])
+                        subdir = os.path.dirname(result['path'])
+                        ext = file_path.split('.')[-1]
+                        label = file_path.split('.')[-2]
+                        file = ".".join([label, ext])
+                        search = os.path.join(self.prj.metadata.output_dir, subdir, '*{}'.format(file))
+                        if glob.glob(search):
+                            file_path = str(glob.glob(search)[0])
+                            file_relpath = os.path.relpath(file_path, self.prj.metadata.output_dir)
+                            ext = img_path.split('.')[-1]
+                            label = img_path.split('.')[-2]
+                            img = ".".join([label, ext])
+                            search = os.path.join(self.prj.metadata.output_dir, subdir, '*{}'.format(img))
+                            if glob.glob(search):
+                                img_path = str(glob.glob(search)[0])
+                                img_relpath = os.path.relpath(img_path, self.prj.metadata.output_dir)
                                 if num_figures < 3:
                                     # Add to single row
                                     obj_figs.append(HTML_FIGURE.format(
-                                        path=file_path_formatted,
-                                        image=thumbnail_path_formatted,
+                                        path=file_relpath,
+                                        image=img_relpath,
                                         label=caption))
                                     num_figures += 1
                                 else:
@@ -1069,17 +1073,16 @@ class Summarizer(Executor):
                                     obj_figs.append("\t\t\t</div>")
                                     obj_figs.append("\t\t\t<div class='row justify-content-start'>")
                                     obj_figs.append(HTML_FIGURE.format(
-                                        path=file_path_formatted,
-                                        image=thumbnail_path_formatted,
+                                        path=file_relpath,
+                                        image=img_relpath,
                                         label=caption))
                             else:
                                 # No thumbnail exists, add as a link in a list
                                 obj_links.append(OBJECTS_LINK.format(
-                                                    path=file_path_formatted,
+                                                    path=file_relpath,
                                                     label=caption))
                         else:
                             _LOGGER.warn("Summarizer was unable to find the: " + caption)
-
             while num_figures < 3:
                 # Add additional empty columns for clean format
                 obj_figs.append("\t\t\t  <div class='col'>")
@@ -1215,7 +1218,7 @@ class Summarizer(Executor):
             objs_html_file.close()
 
             _LOGGER.info(
-                "Summary (n=" + str(len(stats)) + "): " + objs_html_path)
+                "Summary (n=" + str(len(stats)) + "): " + tsv_outfile_path)
 
         # First, the generic summarize will pull together all the fits
         # and stats from each sample into project-combined spreadsheets.
