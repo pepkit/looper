0d3da112cdf3f404240bd0c130d2426e06322b83
jpsmith5 <jasonsmith@virginia.edu>
2018-07-26 14:21:25 -0400

    Fix summarizers NoneType error

--- looper/looper.py
+++ looper/looper.py
@@ -654,14 +654,15 @@ class Summarizer(Executor):
                 _LOGGER.debug(iface)
                 pl = iface.fetch_pipelines(protocol)
                 summarizers = iface.get_attribute(pl, "summarizers")
-                for summarizer in set(summarizers):
-                    summarizer_abspath = os.path.join(
-                        os.path.dirname(iface.pipe_iface_file), summarizer)
-                    _LOGGER.debug([summarizer_abspath, self.prj.config_file])
-                    try:
-                        subprocess.call([summarizer_abspath, self.prj.config_file])
-                    except OSError:
-                        _LOGGER.warn("Summarizer was unable to run: " + str(summarizer))
+                if summarizers is not None:
+                    for summarizer in set(summarizers):
+                        summarizer_abspath = os.path.join(
+                            os.path.dirname(iface.pipe_iface_file), summarizer)
+                        _LOGGER.debug([summarizer_abspath, self.prj.config_file])
+                        try:
+                            subprocess.call([summarizer_abspath, self.prj.config_file])
+                        except OSError:
+                            _LOGGER.warn("Summarizer was unable to run: " + str(summarizer))
 
         # Produce HTML report
         report_builder = HTMLReportBuilder(self.prj)
