a0eb5365b2ba635c06a74f314777fa68ca5a07d4
Vince <vince.reuter@gmail.com>
2019-05-31 12:54:31 -0400

    use helper from ubiquerg

--- looper/looper.py
+++ looper/looper.py
@@ -37,6 +37,7 @@ from divvy import DEFAULT_COMPUTE_RESOURCES_NAME, NEW_COMPUTE_KEY as COMPUTE_KEY
 from logmuse import setup_logger
 from peppy import ProjectContext, METADATA_KEY, SAMPLE_EXECUTION_TOGGLE
 
+from ubiquerg import query_yes_no
 
 SUBMISSION_FAILURE_MESSAGE = "Cluster resource failure"
 
@@ -628,42 +629,6 @@ def create_failure_message(reason, samples):
     reason_text = color + reason + Style.RESET_ALL
     samples_text = ", ".join(samples)
     return "{}: {}".format(reason_text, samples_text)
-
-
-def query_yes_no(question, default="no"):
-    """
-    Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {
-        "yes": True, "y": True, "ye": True,
-        "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write(
-                "Please respond with 'yes' or 'no' "
-                "(or 'y' or 'n').\n")
 
 
 def destroy_sample_results(result_outfolder, args):
