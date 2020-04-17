7dd89eef907e62dbcec28e5d2b9091b149e0a3d4
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-04-17 19:42:15 -0400

    test looper runp, add test files

--- tests/smoketests/conftest.py
+++ tests/smoketests/conftest.py
@@ -7,6 +7,7 @@ CFG = "project_config.yaml"
 ST = "annotation_sheet.csv"
 PI = "pipeline_interface{}.yaml"
 OS = "output_schema.yaml"
+RES = "resources-{}.tsv"
 
 @pytest.fixture
 def example_pep_piface_path():
@@ -30,16 +31,22 @@ def prep_temp_pep(example_pep_piface_path):
     piface1_path = os.path.join(example_pep_piface_path, PI.format("1"))
     piface2_path = os.path.join(example_pep_piface_path, PI.format("2"))
     schema_path = os.path.join(example_pep_piface_path, OS)
+    res_proj_path = os.path.join(example_pep_piface_path, RES.format("project"))
+    res_samp_path = os.path.join(example_pep_piface_path, RES.format("sample"))
     # temp copies
     temp_path_cfg = os.path.join(td, CFG)
     temp_path_sample_table = os.path.join(td, ST)
     temp_path_piface1 = os.path.join(td, PI.format("1"))
     temp_path_piface2 = os.path.join(td, PI.format("2"))
     temp_path_schema = os.path.join(td, OS)
+    temp_path_res_proj = os.path.join(td, RES.format("project"))
+    temp_path_res_samp = os.path.join(td, RES.format("sample"))
     # copying
     cpf(cfg_path, temp_path_cfg)
     cpf(sample_table_path, temp_path_sample_table)
     cpf(piface1_path, temp_path_piface1)
     cpf(piface2_path, temp_path_piface2)
     cpf(schema_path, temp_path_schema)
+    cpf(res_proj_path, temp_path_res_proj)
+    cpf(res_samp_path, temp_path_res_samp)
     return temp_path_cfg
--- tests/smoketests/test_run.py
+++ tests/smoketests/test_run.py
@@ -5,11 +5,12 @@ from looper.const import *
 import subprocess
 from yaml import safe_load, dump
 
-def subp_run(pth):
+
+def subp_exec(pth, cmd):
     """
     looper run in a subprocess, example cfg
     """
-    proc = subprocess.Popen(["looper", "run", "-d", pth],
+    proc = subprocess.Popen(["looper", cmd, "-d", pth],
                             stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     return str(stdout), str(stderr), proc.returncode
@@ -18,11 +19,11 @@ def subp_run(pth):
 class LooperRunTests:
     def test_looper_run_basic(self, example_pep_piface_path_cfg):
         """ Verify looper runs in a basic case and return code is 0 """
-        stdout, stderr, rc = subp_run(example_pep_piface_path_cfg)
+        stdout, stderr, rc = subp_exec(example_pep_piface_path_cfg, "run")
         assert rc == 0
 
     def test_looper_multi_pipeline(self, example_pep_piface_path_cfg):
-        stdout, stderr, rc = subp_run(example_pep_piface_path_cfg)
+        stdout, stderr, rc = subp_exec(example_pep_piface_path_cfg, "run")
         assert "Commands submitted: 6 of 6" in stderr
 
     def test_looper_single_pipeline(self, prep_temp_pep):
@@ -35,6 +36,31 @@ class LooperRunTests:
         # print("\nconfig_data: \n{}\n".format(config_data))
         with open(tp, 'w') as conf_file:
             dump(config_data, conf_file)
-        stdout, stderr, rc = subp_run(tp)
+        stdout, stderr, rc = subp_exec(tp, "run")
         assert rc == 0
         assert "Commands submitted: 6 of 6" not in stderr
+
+
+class LooperRunpTests:
+    def test_looper_runp_basic(self, example_pep_piface_path_cfg):
+        """ Verify looper runps in a basic case and return code is 0 """
+        stdout, stderr, rc = subp_exec(example_pep_piface_path_cfg, "runp")
+        assert rc == 0
+
+    def test_looper_multi_pipeline(self, example_pep_piface_path_cfg):
+        stdout, stderr, rc = subp_exec(example_pep_piface_path_cfg, "runp")
+        assert "Jobs submitted: 2" in stderr
+
+    def test_looper_single_pipeline(self, prep_temp_pep):
+        tp = prep_temp_pep
+        with open(tp, 'r') as conf_file:
+            config_data = safe_load(conf_file)
+        # print("\nconfig_data: \n{}\n".format(config_data))
+        pifaces = config_data[SAMPLE_MODS_KEY][CONSTANT_KEY][PIPELINE_INTERFACES_KEY]
+        config_data[SAMPLE_MODS_KEY][CONSTANT_KEY][PIPELINE_INTERFACES_KEY] = pifaces[1]
+        # print("\nconfig_data: \n{}\n".format(config_data))
+        with open(tp, 'w') as conf_file:
+            dump(config_data, conf_file)
+        stdout, stderr, rc = subp_exec(tp, "runp")
+        assert rc == 0
+        assert "Jobs submitted: 2" not in stderr
