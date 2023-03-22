400b7141ef65a2638ef6c99fbc4d795ee1cc3743
Vince Reuter <vince.reuter@gmail.com>
2023-03-22 15:18:54 +0100

    support 3.8 - 3.11 and only test bookend versions; close #347

--- setup.py
+++ setup.py
@@ -67,10 +67,10 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     keywords="bioinformatics, sequencing, ngs",
