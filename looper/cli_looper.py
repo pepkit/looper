23e151fc695cfea0d1016ee1d41009f2637cdc82
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2023-12-15 10:05:44 -0500

    basic tab completion for initial commands #422

--- looper/cli_looper.py
+++ looper/cli_looper.py
@@ -92,6 +92,8 @@ def build_parser():
             help="Turn on debug mode (default: %(default)s)",
         )
 
+
+
         parser = logmuse.add_logging_options(parser)
         subparsers = parser.add_subparsers(dest="command")
 
@@ -495,6 +497,11 @@ def build_parser():
             help="Number of attributes to display",
             type=int,
         )
+        parser.add_argument(
+            "--commands",
+            action="version",
+            version="{}".format(" ".join(subparsers.choices.keys())))
+
         result.append(parser)
     return result
 
