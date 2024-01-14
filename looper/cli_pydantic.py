bed82923e3fb596a5927049b242ecafac77e3418
Simeon Carstens <simeon.carstens@tweag.io>
2024-01-14 19:57:27 +0100

    Rework design
    - make arguments inherit from `pydantic.fields.FieldInfo` and augment
    them with a `name` attribute
    - make a `Command` class that generates a `pydantic` model with the
    arguments the command takes
    - make commands take lists of specific arguments they take instead of
    having arguments know by which commands they are supported

--- looper/cli_pydantic.py
+++ looper/cli_pydantic.py
@@ -1,124 +1,139 @@
 from dataclasses import dataclass
 import enum
-import os
-from typing import Optional, TypeAlias
+from typing import Any, Optional
 
 import pydantic
 import pydantic_argparse
 
-from const import MESSAGE_BY_SUBCOMMAND
+from .const import MESSAGE_BY_SUBCOMMAND
 
-AllowedArgumentType: TypeAlias = str | int | bool | list
 
-class Command(enum.Enum):
-    """
-    Lists all supported commands
-    """
-    RUN = enum.auto()
-
-@dataclass
-class Argument:
+class Argument(pydantic.fields.FieldInfo):
     """
     CLI argument / flag definition
+
+    Naively, one would think one could just subclass `pydantic.Field`,
+    but actually `pydantic.Field` is a function, and not a class.
+    `pydantic.Field()` returns a validated `FieldInfo` instance,
+    so we instead sublcass `FieldInfo` directly and validate it in the
+    constructor.
+
+    :param str name: argument name, e.g. "ignore-args"
+    :param str description: argument description, which will appear as the
+        help text for this argument
+    :param Any default: a tuple of the form (type, default_value). If the
+        default value is `...` (Ellipsis), then the argument is required.
     """
-    # argument name, e.g. "ignore-args"
+
+    def __init__(self, name: str, default: Any, description: str) -> None:
+        self._name = name
+        super().__init__(default=default, description=description)
+        self._validate()
+
+    @property
+    def name(self):
+        """
+        Argument name as used in the CLI, e.g. "ignore-args"
+        """
+        return self._name
+
+@dataclass
+class Command:
+    """
+    Representation of a command
+
+    :param str name: command name
+    :param str description: command description
+    :param list[Argument] arguments: list of arguments supported by this command
+    """
     name: str
-    # argument type, e.g. `bool`
-    type: type[AllowedArgumentType]
-    # argument description (will be the CLI help text)
     description: str
-    # default value for argument (needs to be an instance of `type`)
-    # TODO: how can we constrain the type of this to be an instance of
-    # the value of the `type` field?
-    default: None | AllowedArgumentType
-    # set of commands this argument is used by
-    used_by: set[Command]
+    arguments: list[Argument]
 
-    def __post_init__(self):
-        if self.default is not None and not isinstance(self.default, self.type):
-            raise TypeError(
-                "Value for `default` needs to be of the type given in "
-                f"the `type` field ({self.type})"
-                )
+    def create_model(self) -> type[pydantic.BaseModel]:
+        """
+        Creates a `pydantic` model for this command
+        """
+        arguments = dict()
+        for arg in self.arguments:
+            # These gymnastics are necessary because of
+            # https://github.com/pydantic/pydantic/issues/2248#issuecomment-757448447
+            arg_type, arg_default_value = arg.default
+            arguments[arg.name] = (
+                arg_type,
+                pydantic.Field(arg_default_value, description=arg.description)
+            )
+        return pydantic.create_model(
+            self.name, **arguments
+        )
 
-arguments = [
-    Argument(
+class ArgumentEnum(enum.Enum):
+    """
+    Lists all available arguments
+
+    TODO: not sure whether an enum is the ideal data structure for that
+    """
+    IGNORE_FLAGS = Argument(
         "ignore-flags",
-        bool,
+        (bool, False),
         "Ignore run status flags",
-        False,
-        {Command.RUN}
-    ),
-    Argument(
-        "time-delay",
-        int,
-        "Time delay in seconds between job submissions (min: 0, max: 30)",
-        0,
-        {Command.RUN}
-    ),
-    Argument(
-        "dry-run",
-        bool,
-        "Don't actually submit jobs",
-        False,
-        {Command.RUN}
-    ),
-    Argument(
-        "command-extra",
-        str,
-        "String to append to every command",
-        "",
-        {Command.RUN}
-    ),
-    Argument(
-        "command-extra-override",
-        str,
-        "Same as command-extra, but overrides values in PEP",
-        "",
-        {Command.RUN}
-    ),
-    Argument(
-        "lump",
-        int,
-        "Total input file size (GB) to batch into one job",
-        None,
-        {Command.RUN}
-    ),
-    Argument(
-        "lumpn",
-        int,
-        "Number of commands to batch into one job",
-        None,
-        {Command.RUN}
-    ),
-    Argument(
-        "limit",
-        int,
-        "Limit to n samples",
-        None,
-        {Command.RUN}
-    ),
-    Argument(
-        "skip",
-        int,
-        "Skip samples by numerical index",
-        None,
-        {Command.RUN}
     )
-]
+    TIME_DELAY = Argument(
+        "time-delay",
+        (int, 0),
+        "Time delay in seconds between job submissions (min: 0, max: 30)",
+    )
+    DRY_RUN = Argument(
+        "dry-run",
+        (bool, False),
+        "Don't actually submit jobs"
+    )
+    COMMAND_EXTRA = Argument(
+        "command-extra",
+        (str, ""),
+        "String to append to every command"
+    )
+    COMMAND_EXTRA_OVERRIDE = Argument(
+        "command-extra-override",
+        (str, ""),
+        "Same as command-extra, but overrides values in PEP"
+    )
+    LUMP = Argument(
+        "lump",
+        (int, None),
+        "Total input file size (GB) to batch into one job"
+    )
+    LUMPN = Argument(
+        "lumpn",
+        (int, None),
+        "Number of commands to batch into one job"
+    )
+    LIMIT = Argument(
+        "limit",
+        (int, None),
+        "Limit to n samples"
+    )
+    SKIP = Argument(
+        "skip",
+        (int, None),
+        "Skip samples by numerical index"
+    )
 
-def create_model_from_arguments(name: str, command: Command, arguments: list[Argument]) -> type[pydantic.BaseModel]:
-    """
-    Creates a `pydantic` model for a command from a list of arguments
-    """
-    return pydantic.create_model(
-    name, **{
-        arg.name: (arg.type, pydantic.Field(description=arg.description, default=arg.default))
-        for arg in arguments if command in arg.used_by
-    }
+RunParser = Command(
+    "run", MESSAGE_BY_SUBCOMMAND["run"],
+    [
+        ArgumentEnum.IGNORE_FLAGS.value,
+        ArgumentEnum.TIME_DELAY.value,
+        ArgumentEnum.DRY_RUN.value,
+        ArgumentEnum.COMMAND_EXTRA.value,
+        ArgumentEnum.COMMAND_EXTRA_OVERRIDE.value,
+        ArgumentEnum.LUMP.value,
+        ArgumentEnum.LUMPN.value,
+        ArgumentEnum.LIMIT.value,
+        ArgumentEnum.SKIP.value
+    ]
 )
-
-RunParser = create_model_from_arguments("RunParser", Command.RUN, arguments)
+RunParserModel = RunParser.create_model()
 
 class TopLevelParser(pydantic.BaseModel):
     """
@@ -127,31 +142,7 @@ class TopLevelParser(pydantic.BaseModel):
     - arguments that are required no matter the subcommand
     """
     # commands
-    run: Optional[RunParser] = pydantic.Field(description=MESSAGE_BY_SUBCOMMAND["run"])
-
-    # top-level arguments
-    config_file: Optional[str] = pydantic.Field(
-        description="Project configuration file"
-    )
-    pep_config: Optional[str] = pydantic.Field(description="PEP configuration file")
-    output_dir: Optional[str] = pydantic.Field(description="Output directory")
-    sample_pipeline_interfaces: Optional[str] = pydantic.Field(
-        description="Sample pipeline interfaces definition"
-    )
-    project_pipeline_interfaces: Optional[str] = pydantic.Field(
-        description="Project pipeline interfaces definition"
-    )
-    amend: Optional[bool] = pydantic.Field(description="List of amendments to activate")
-    sel_flag: Optional[bool] = pydantic.Field(description="Selection flag")
-    exc_flag: Optional[bool] = pydantic.Field(description="Exclusion flag")
-    divvy: Optional[str] = pydantic.Field(
-        description=(
-            "Path to divvy configuration file. Default=$DIVCFG env "
-            "variable. Currently: {}".format(os.getenv("DIVCFG", None) or "not set")
-        )
-    )
-
-
+    run: Optional[RunParserModel] = pydantic.Field(description=RunParser.description)
 
 
 def main() -> None:
