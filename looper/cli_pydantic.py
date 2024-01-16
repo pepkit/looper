bc5c78fd307543727b7eaa21a657bd7b33e26fd4
Zhihan Zhang <zhihan.zhang@tweag.io>
2024-01-16 12:55:36 +0800

    Use **kwargs in Argument and ArgumentEnum

--- looper/cli_pydantic.py
+++ looper/cli_pydantic.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
 import enum
-from typing import Any, Optional
+from dataclasses import dataclass
+from typing import Optional
 
 import pydantic
 import pydantic_argparse
@@ -19,15 +19,13 @@ class Argument(pydantic.fields.FieldInfo):
     constructor.
 
     :param str name: argument name, e.g. "ignore-args"
-    :param str description: argument description, which will appear as the
-        help text for this argument
-    :param Any default: a tuple of the form (type, default_value). If the
-        default value is `...` (Ellipsis), then the argument is required.
+    :param dict kwargs: additional keyword arguments supported by
+        `FieldInfo`, such as description, default value, etc.
     """
 
-    def __init__(self, name: str, default: Any, description: str) -> None:
+    def __init__(self, name, **kwargs) -> None:
         self._name = name
-        super().__init__(default=default, description=description)
+        super().__init__(**kwargs)
         self._validate()
 
     @property
@@ -36,6 +34,7 @@ class Argument(pydantic.fields.FieldInfo):
         Argument name as used in the CLI, e.g. "ignore-args"
         """
         return self._name
+
 
 @dataclass
 class Command:
@@ -46,6 +45,7 @@ class Command:
     :param str description: command description
     :param list[Argument] arguments: list of arguments supported by this command
     """
+
     name: str
     description: str
     arguments: list[Argument]
@@ -61,11 +61,10 @@ class Command:
             arg_type, arg_default_value = arg.default
             arguments[arg.name] = (
                 arg_type,
-                pydantic.Field(arg_default_value, description=arg.description)
+                pydantic.Field(arg_default_value, description=arg.description),
             )
-        return pydantic.create_model(
-            self.name, **arguments
-        )
+        return pydantic.create_model(self.name, **arguments)
+
 
 class ArgumentEnum(enum.Enum):
     """
@@ -73,54 +72,51 @@ class ArgumentEnum(enum.Enum):
 
     TODO: not sure whether an enum is the ideal data structure for that
     """
+
     IGNORE_FLAGS = Argument(
-        "ignore-flags",
-        (bool, False),
-        "Ignore run status flags",
+        name="ignore-flags",
+        default=(bool, False),
+        description="Ignore run status flags",
     )
     TIME_DELAY = Argument(
-        "time-delay",
-        (int, 0),
-        "Time delay in seconds between job submissions (min: 0, max: 30)",
+        name="time-delay",
+        default=(int, 0),
+        description="Time delay in seconds between job submissions (min: 0, max: 30)",
     )
     DRY_RUN = Argument(
-        "dry-run",
-        (bool, False),
-        "Don't actually submit jobs"
+        name="dry-run", default=(bool, False), description="Don't actually submit jobs"
     )
     COMMAND_EXTRA = Argument(
-        "command-extra",
-        (str, ""),
-        "String to append to every command"
+        name="command-extra",
+        default=(str, ""),
+        description="String to append to every command",
     )
     COMMAND_EXTRA_OVERRIDE = Argument(
-        "command-extra-override",
-        (str, ""),
-        "Same as command-extra, but overrides values in PEP"
+        name="command-extra-override",
+        default=(str, ""),
+        description="Same as command-extra, but overrides values in PEP",
     )
     LUMP = Argument(
-        "lump",
-        (int, None),
-        "Total input file size (GB) to batch into one job"
+        name="lump",
+        default=(int, None),
+        description="Total input file size (GB) to batch into one job",
     )
     LUMPN = Argument(
-        "lumpn",
-        (int, None),
-        "Number of commands to batch into one job"
+        name="lumpn",
+        default=(int, None),
+        description="Number of commands to batch into one job",
     )
     LIMIT = Argument(
-        "limit",
-        (int, None),
-        "Limit to n samples"
+        name="limit", default=(int, None), description="Limit to n samples"
     )
     SKIP = Argument(
-        "skip",
-        (int, None),
-        "Skip samples by numerical index"
+        name="skip", default=(int, None), description="Skip samples by numerical index"
     )
 
+
 RunParser = Command(
-    "run", MESSAGE_BY_SUBCOMMAND["run"],
+    "run",
+    MESSAGE_BY_SUBCOMMAND["run"],
     [
         ArgumentEnum.IGNORE_FLAGS.value,
         ArgumentEnum.TIME_DELAY.value,
@@ -130,10 +126,11 @@ RunParser = Command(
         ArgumentEnum.LUMP.value,
         ArgumentEnum.LUMPN.value,
         ArgumentEnum.LIMIT.value,
-        ArgumentEnum.SKIP.value
-    ]
+        ArgumentEnum.SKIP.value,
+    ],
 )
 RunParserModel = RunParser.create_model()
+
 
 class TopLevelParser(pydantic.BaseModel):
     """
@@ -141,6 +138,7 @@ class TopLevelParser(pydantic.BaseModel):
     - commands (run, runp, check...)
     - arguments that are required no matter the subcommand
     """
+
     # commands
     run: Optional[RunParserModel] = pydantic.Field(description=RunParser.description)
 
@@ -154,6 +152,7 @@ def main() -> None:
     )
     args = parser.parse_typed_args()
     print(args)
+
 
 if __name__ == "__main__":
     main()
