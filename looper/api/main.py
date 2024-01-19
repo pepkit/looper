eab51271039a0cfcd8887b4eaef630483880fbe0
Zhihan Zhang <zhihan.zhang@tweag.io>
2024-01-19 12:13:35 +0800

    Create an argparse.Namespace

--- looper/api/main.py
+++ looper/api/main.py
@@ -1,10 +1,21 @@
+from argparse import Namespace
+
 from fastapi import FastAPI
 from looper.command_models.commands import RunParserModel
 
 app = FastAPI(validate_model=True)
 
 
+def create_argparse_namespace(run_model: RunParserModel) -> Namespace:
+    # Create an argparse namespace from the submitted run model
+    namespace = Namespace()
+    for arg in vars(run_model):
+        setattr(namespace, arg, getattr(run_model, arg))
+    return namespace
+
+
 @app.post("/run")
 async def run_endpoint(run_model: RunParserModel):
-    print(run_model)
+    argparse_namespace = create_argparse_namespace(run_model)
+    print(argparse_namespace)
     return run_model
