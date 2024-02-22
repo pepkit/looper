ad020838dfcc8b06e724e6b7338342fabd99af83
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-02-22 15:42:42 -0500

    Add cloning hello looper and executing run #464

--- tests/test_comprehensive.py
+++ tests/test_comprehensive.py
@@ -7,8 +7,13 @@ from looper.project import Project
 from tests.conftest import *
 from looper.utils import *
 from looper.cli_looper import main
+from tests.smoketests.test_run import is_connected
+from tempfile import TemporaryDirectory
+from git import Repo
 
 CMD_STRS = ["string", " --string", " --sjhsjd 212", "7867#$@#$cc@@"]
+
+REPO_URL = "https://github.com/pepkit/hello_looper.git"
 
 
 def test_comprehensive_looper_no_pipestat(prep_temp_pep):
@@ -21,15 +26,24 @@ def test_comprehensive_looper_no_pipestat(prep_temp_pep):
         raise pytest.fail("DID RAISE {0}".format(Exception))
 
 
-def test_comprehensive_looper_pipestat(prep_temp_pep_pipestat):
-    tp = prep_temp_pep_pipestat
+@pytest.mark.skipif(not is_connected(), reason="Test needs an internet connection")
+def test_comprehensive_looper_pipestat():
+    """
+    This test clones the hello_looper repository and runs the looper config file in the pipestat sub-directory
+    """
+
     cmd = "run"
 
-    x = [cmd, "-d", "--looper-config", tp]
+    with TemporaryDirectory() as d:
+        repo = Repo.clone_from(REPO_URL, d)
 
-    try:
-        result = main(test_args=x)
-        if cmd == "run":
-            assert result["Pipestat compatible"] is True
-    except Exception:
-        raise pytest.fail("DID RAISE {0}".format(Exception))
+        path_to_looper_config = os.path.join(d, "pipestat", ".looper.yaml")
+
+        x = [cmd, "-d", "--looper-config", path_to_looper_config]
+
+        try:
+            result = main(test_args=x)
+            if cmd == "run":
+                assert result["Pipestat compatible"] is True
+        except Exception:
+            raise pytest.fail("DID RAISE {0}".format(Exception))
