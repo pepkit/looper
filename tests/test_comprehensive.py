d4cfe2362ea2244382f8201aae342acf16b80651
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-02-26 11:50:57 -0500

    Modify derived attribute path before running samples #464

--- tests/test_comprehensive.py
+++ tests/test_comprehensive.py
@@ -11,6 +11,8 @@ from tests.smoketests.test_run import is_connected
 from tempfile import TemporaryDirectory
 from git import Repo
 
+from yaml import dump, safe_load
+
 CMD_STRS = ["string", " --string", " --sjhsjd 212", "7867#$@#$cc@@"]
 
 REPO_URL = "https://github.com/pepkit/hello_looper.git"
@@ -35,15 +37,29 @@ def test_comprehensive_looper_pipestat():
     cmd = "run"
 
     with TemporaryDirectory() as d:
-        repo = Repo.clone_from(REPO_URL, d)
-
-        # TODO executing either the .py or the .sh pipeline does not correctly report results because pytest is running from a different directory
+        repo = Repo.clone_from(REPO_URL, d, branch="dev_derive")
         pipestat_dir = os.path.join(d, "pipestat")
-        os.chdir(pipestat_dir)
 
-        path_to_looper_config = os.path.join(pipestat_dir, ".looper_pipestat_shell.yaml")
+        path_to_looper_config = os.path.join(
+            pipestat_dir, ".looper_pipestat_shell.yaml"
+        )
 
-        x = [cmd, "-d", "--looper-config", path_to_looper_config]
+        # open up the project config and replace the derived attributes with the path to the data. In a way, this simulates using the environment variables.
+        pipestat_project_file = os.path.join(
+            d, "pipestat/project", "project_config.yaml"
+        )
+        with open(pipestat_project_file, "r") as f:
+            pipestat_project_data = safe_load(f)
+
+        pipestat_project_data["sample_modifiers"]["derive"]["sources"]["source1"] = (
+            os.path.join(pipestat_dir, "data/{sample_name}.txt")
+        )
+
+        with open(pipestat_project_file, "w") as f:
+            dump(pipestat_project_data, f)
+
+        # x = [cmd, "-d", "--looper-config", path_to_looper_config]
+        x = [cmd, "--looper-config", path_to_looper_config]
 
         try:
             result = main(test_args=x)
