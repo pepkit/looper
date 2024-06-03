f476e68c9b3b589350dbc3929393f8b891e0fab4
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-06-03 10:32:10 -0400

    remove commented code

--- looper/looper.py
+++ looper/looper.py
@@ -91,12 +91,6 @@ class Checker(Executor):
 
         psms = {}
         if getattr(args, "project", None):
-            # psms = self.prj.get_pipestat_managers(project_level=True)
-            # for pipeline_name, psm in psms.items():
-            #     s = psm.get_status() or "unknown"
-            #     status.setdefault(pipeline_name, {})
-            #     status[pipeline_name][self.prj.name] = s
-            #     _LOGGER.debug(f"{self.prj.name} ({pipeline_name}): {s}")
 
             for piface in self.prj.pipeline_interfaces:
                 if piface.psm.pipeline_type == "project":
@@ -117,12 +111,6 @@ class Checker(Executor):
                         _LOGGER.debug(
                             f"{sample.sample_name} ({piface.psm.pipeline_name}): {s}"
                         )
-                # psms = self.prj.get_pipestat_managers(sample_name=sample.sample_name)
-                # for pipeline_name, psm in psms.items():
-                #     s = psm.get_status(record_identifier=sample.sample_name)
-                #     status.setdefault(pipeline_name, {})
-                #     status[pipeline_name][sample.sample_name] = s
-                #     _LOGGER.debug(f"{sample.sample_name} ({pipeline_name}): {s}")
 
         console = Console()
 
@@ -349,18 +337,6 @@ class Collator(Executor):
             )
         self.counter = LooperCounter(len(project_pifaces))
         for project_piface in project_pifaces:
-            # try:
-            #     project_piface_object = PipelineInterface(
-            #         project_piface, pipeline_type="project"
-            #     )
-            # except (IOError, ValidationError) as e:
-            #     _LOGGER.warning(
-            #         "Ignoring invalid pipeline interface source: {}. "
-            #         "Caught exception: {}".format(
-            #             project_piface, getattr(e, "message", repr(e))
-            #         )
-            #     )
-            #     continue
             _LOGGER.info(
                 self.counter.show(
                     name=self.prj.name,
@@ -586,13 +562,6 @@ class Reporter(Executor):
         psms = {}
 
         if project_level:
-            # psms = self.prj.get_pipestat_managers(project_level=True)
-            # print(psms)
-            # for name, psm in psms.items():
-            #     # Summarize will generate the static HTML Report Function
-            #     report_directory = psm.summarize(
-            #         looper_samples=self.prj.samples, portable=portable
-            #     )
 
             for piface in self.prj.pipeline_interfaces:
                 if piface.psm.pipeline_type == "project":
@@ -612,24 +581,6 @@ class Reporter(Executor):
                     )
                     print(f"Report directory: {report_directory}")
                     self.debug["report_directory"] = report_directory
-
-            # for piface_source_samples in self.prj._samples_by_piface(
-            #     self.prj.piface_key
-            # ).values():
-            #     # For each piface_key, we have a list of samples, but we only need one sample from the list to
-            #     # call the related pipestat manager object which will pull ALL samples when using psm.summarize
-            #     first_sample_name = list(piface_source_samples)[0]
-            #     psms = self.prj.get_pipestat_managers(
-            #         sample_name=first_sample_name, project_level=False
-            #     )
-            #     print(psms)
-            #     for name, psm in psms.items():
-            #         # Summarize will generate the static HTML Report Function
-            #         report_directory = psm.summarize(
-            #             looper_samples=self.prj.samples, portable=portable
-            #         )
-            #         print(f"Report directory: {report_directory}")
-            #         self.debug["report_directory"] = report_directory
             return self.debug
 
 
@@ -650,23 +601,7 @@ class Linker(Executor):
                     psms[piface.psm.pipeline_name] = piface.psm
                     linked_results_path = piface.psm.link(link_dir=link_dir)
                     print(f"Linked directory: {linked_results_path}")
-            # psms = self.prj.get_pipestat_managers(project_level=True)
-            # for name, psm in psms.items():
-            #     linked_results_path = psm.link(link_dir=link_dir)
-            #     print(f"Linked directory: {linked_results_path}")
         else:
-            # for piface_source_samples in self.prj._samples_by_piface(
-            #     self.prj.piface_key
-            # ).values():
-            #     # For each piface_key, we have a list of samples, but we only need one sample from the list to
-            #     # call the related pipestat manager object which will pull ALL samples when using psm.summarize
-            #     first_sample_name = list(piface_source_samples)[0]
-            #     psms = self.prj.get_pipestat_managers(
-            #         sample_name=first_sample_name, project_level=False
-            #     )
-            #     for name, psm in psms.items():
-            #         linked_results_path = psm.link(link_dir=link_dir)
-            #         print(f"Linked directory: {linked_results_path}")
             for piface in self.prj.pipeline_interfaces:
                 if piface.psm.pipeline_type == "sample":
                     psms[piface.psm.pipeline_name] = piface.psm
@@ -690,21 +625,7 @@ class Tabulator(Executor):
                 if piface.psm.pipeline_type == "project":
                     psms[piface.psm.pipeline_name] = piface.psm
                     results = piface.psm.table()
-            # psms = self.prj.get_pipestat_managers(project_level=True)
-            # for name, psm in psms.items():
-            #     results = psm.table()
         else:
-            # for piface_source_samples in self.prj._samples_by_piface(
-            #     self.prj.piface_key
-            # ).values():
-            #     # For each piface_key, we have a list of samples, but we only need one sample from the list to
-            #     # call the related pipestat manager object which will pull ALL samples when using psm.table
-            #     first_sample_name = list(piface_source_samples)[0]
-            #     psms = self.prj.get_pipestat_managers(
-            #         sample_name=first_sample_name, project_level=False
-            #     )
-            #     for name, psm in psms.items():
-            #         results = psm.table()
             for piface in self.prj.pipeline_interfaces:
                 if piface.psm.pipeline_type == "sample":
                     psms[piface.psm.pipeline_name] = piface.psm
@@ -780,13 +701,6 @@ def destroy_summary(prj, dry_run=False, project_level=False):
         for piface in prj.pipeline_interfaces:
             if piface.psm.pipeline_type == "sample":
                 psms[piface.psm.pipeline_name] = piface.psm
-        # for piface_source_samples in prj._samples_by_piface(prj.piface_key).values():
-        #     # For each piface_key, we have a list of samples, but we only need one sample from the list to
-        #     # call the related pipestat manager object which will pull ALL samples when using psm.table
-        #     first_sample_name = list(piface_source_samples)[0]
-        #     psms = prj.get_pipestat_managers(
-        #         sample_name=first_sample_name, project_level=False
-        #     )
         for name, psm in psms.items():
             _remove_or_dry_run(
                 [
