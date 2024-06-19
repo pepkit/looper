93e8d31848658cc45b8c76199bc63ea4c78d4465
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-06-19 11:34:47 -0400

    use deep_update for sample modifiers, bump ubiquerg version req

--- looper/project.py
+++ looper/project.py
@@ -20,7 +20,7 @@ from peppy import CONFIG_KEY, OUTDIR_KEY
 from peppy import Project as peppyProject
 from peppy.utils import make_abs_via_cfg
 from pipestat import PipestatError, PipestatManager
-from ubiquerg import expandpath, is_command_callable
+from ubiquerg import expandpath, is_command_callable, deep_update
 from yacman import YAMLConfigManager
 from .conductor import write_pipestat_config
 
@@ -138,10 +138,12 @@ class Project(peppyProject):
         # add sample pipeline interface to the project
         if kwargs.get(SAMPLE_MODS_KEY) and self._modifier_exists():
             _LOGGER.warning(
-                "Sample modifiers were provided in Looper config. Overwriting those in  PEP."
+                "Sample modifiers were provided in Looper Config and in PEP Project Config. Merging..."
             )
-            self.config.setdefault("sample_modifiers", {})
-            self.config["sample_modifiers"] = kwargs.get(SAMPLE_MODS_KEY)
+            deep_update(self.config["sample_modifiers"], kwargs.get(SAMPLE_MODS_KEY))
+            _LOGGER.debug(
+                msg=f"Merged sample modifiers: {self.config['sample_modifiers']}"
+            )
         elif kwargs.get(SAMPLE_MODS_KEY):
             self.config.setdefault("sample_modifiers", {})
             self.config["sample_modifiers"] = kwargs.get(SAMPLE_MODS_KEY)
