4c9549bb1fa62d23e50107ebf12b6f946b21565c
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-08-20 10:40:58 -0400

    fix #511

--- looper/__main__.py
+++ looper/__main__.py
@@ -5,7 +5,7 @@ from .cli_divvy import main as divvy_main
 
 if __name__ == "__main__":
     try:
-        sys.exit(main())
+        main()
     except KeyboardInterrupt:
         print("Program canceled by user!")
         sys.exit(1)
